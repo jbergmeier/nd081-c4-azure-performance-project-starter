7c8e4e0a322cd83784f37c7bb11469503fa4e129
Joern Bergmeier <joern.bergmeier@arvato.com>
2021-07-07 12:47:40 +0200

    added main change

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -119,11 +119,15 @@ def index():
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
             logger.warning('Cats Vote', extra=properties)
+            print(r)
+            
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
             logger.warning('Dogs Vote', extra=properties)
+            print("Vote1: " + vote1)
+            print("Vote2: " + vote2)
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
         else:
