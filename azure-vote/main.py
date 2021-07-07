97ee2519ef8cd495d2b1cf7d0bc3cc70ede5d1ec
Joern Bergmeier <joern.bergmeier@arvato.com>
2021-07-07 12:57:16 +0200

    new changes

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -119,14 +119,13 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
-            print(properties)
             logger.warning('Cats Vote', extra=properties)
   
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
-            print(properties)
+            print("Properties: " + properties)
             logger.warning('Dogs Vote', extra=properties)
            
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -138,22 +137,28 @@ def index():
             r.incr(vote,1)
 
             # Get current values
+            #vote1 = r.get(button1).decode('utf-8')
+            #vote2 = r.get(button2).decode('utf-8')
+            
+
+            # Get current values
             vote1 = r.get(button1).decode('utf-8')
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            # TODO: use logger object to log cat vote
+            print("Properties: " + properties)
+
+            logger.warning('Cats Vote', extra=properties)
+
             vote2 = r.get(button2).decode('utf-8')
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            # TODO: use logger object to log dog vote
+            print("Properties: " + properties)
+
+            logger.warning('Dogs Vote', extra=properties) 
+
             print("Vote1: " + vote1)
             print("Vote2: " + vote2)
 
-            # Get current values
-            #vote1 = r.get(button1).decode('utf-8')
-            #properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            # TODO: use logger object to log cat vote
-            #logger.warning('Cats Vote', extra=properties)
-
-            #vote2 = r.get(button2).decode('utf-8')
-            #properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            # TODO: use logger object to log dog vote
-            #logger.warning('Dogs Vote', extra=properties)    
-            
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
